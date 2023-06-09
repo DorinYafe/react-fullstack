5c223507147f4aba9007ef8b06fc13184ace3b40
Dorin Yafe <dorinyafe@gmail.com>
2023-06-09 17:06:15 +0300

    add routes

--- server/index.js
+++ server/index.js
@@ -3,6 +3,12 @@ const app = express();
 
 const db = require('./models');
 
+app.use(express.json());
+
+// Routes
+const postsRouter = require('./routes/Posts');
+app.use('/posts', postsRouter);
+
 db.sequelize.sync().then(() => {
   app.listen(3001, () => {
     console.log('Server started on port 3001');
